aa8d4f38e3afbc40fb2bb92a5fbb41c29bc1a070
josh <josh@durga.serve.com>
2010-04-24 23:54:31 +0400

    changed the case of the list name (as returned) and handle the case of a broadcast to a non-existance list

--- lib/switchboard/message_handlers/incoming/demo_list_message_handler.rb
+++ lib/switchboard/message_handlers/incoming/demo_list_message_handler.rb
@@ -9,21 +9,28 @@ module Switchboard::MessageHandlers::Incoming
             messages_to_handle.each do |message|
                 tokens = message.body.split(/ /)
                 list_name = tokens[0].upcase
-                list = List.find_or_create_by_name(list_name)
                  
                 if (tokens.length == 1)
+                    list = List.find_or_create_by_name(list_name)
                     num = PhoneNumber.create!(:number => message.from )
                     list.add_phone_number(num)
-                    create_outgoing_message( message.from, "You have joined the text message list called '" + tokens[0] + "'!" )
+                    create_outgoing_message( message.from, "You have joined the text message list called '" + listname + "'!" )
+                    handled_state.messages.push(message)
                 end
             
                 if (tokens.length > 1) 
-                    list.phone_numbers.each do |phone_number|
-                        body = '[' + list_name + '] ' + tokens[1..-1].join(' ')
-                        create_outgoing_message(phone_number.number, body)
+                    if List.exists?({name => list_name}) 
+                        list.find_by_name(list_name)
+                        list.phone_numbers.each do |phone_number|
+                            body = '[' + list_name + '] ' + tokens[1..-1].join(' ')
+                            create_outgoing_message(phone_number.number, body)
+                        end
+                        handled_state.messages.push(message)
+                    else 
+                        create_outgoing_message(message.from, "It seems like you were trying to send a message to the list called " + list_name + ", but no one has started that list yet."
+                        handled_state.messages.push(message)
                     end
                 end 
-                handled_state.messages.push(message)
                 handled_state.save
             end
         end
