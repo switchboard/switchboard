33120e711ab6e2eb69c14f67bfaa98d4b4f6265b
josh <josh@durga.serve.com>
2010-04-24 23:49:13 +0400

    removed comments

--- script/test/process_incoming_messages.rb
+++ script/test/process_incoming_messages.rb
@@ -9,11 +9,6 @@ outgoing_state = MessageState.find_or_create_by_name('sent');
 require 'switchboard/message_handlers/incoming/demo_list_message_handler.rb'
 require 'switchboard/message_handlers/outgoing/twilio_outgoing_message_handler.rb'
 incoming_state = MessageState.find_by_name('incoming')
-#m = Message.new
-#m.body = "demo This is the second thingo to the dmeo list"
-#m.from = '2679777848'
-#incoming_state.messages.push(m)
-#incoming_state.save
 
 incoming = Switchboard::MessageHandlers::Incoming::DemoListMessageHandler.new
 incoming.handle_messages!
