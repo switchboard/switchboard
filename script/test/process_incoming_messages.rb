08074a9d447a03b1d055d931966581f09d3e4066
josh <josh@durga.serve.com>
2010-06-11 00:06:28 +0400

    updated for email gateway

--- script/test/process_incoming_messages.rb
+++ script/test/process_incoming_messages.rb
@@ -7,11 +7,11 @@ outgoing_state = MessageState.find_or_create_by_name('handled');
 outgoing_state = MessageState.find_or_create_by_name('sent');
 
 require 'switchboard/message_handlers/incoming/demo_list_message_handler.rb'
-require 'switchboard/message_handlers/outgoing/twilio_outgoing_message_handler.rb'
+require 'switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb'
 
 incoming = Switchboard::MessageHandlers::Incoming::DemoListMessageHandler.new
 
-outgoing = Switchboard::MessageHandlers::Outgoing::TwilioOutgoingMessageHandler.new
+outgoing = Switchboard::MessageHandlers::Outgoing::TwilioOrEmailOutgoingMessageHandler.new
 while true
     incoming.handle_messages!
     outgoing.handle_messages!
