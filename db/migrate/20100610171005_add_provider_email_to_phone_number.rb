185e3166056fdfb4eb844252d10cbcb565642ade
josh <josh@durga.serve.com>
2010-06-11 01:15:58 +0400

    bugfix

--- db/migrate/20100610171005_add_provider_email_to_phone_number.rb
+++ db/migrate/20100610171005_add_provider_email_to_phone_number.rb
@@ -1,4 +1,4 @@
-class AddProviderEmailAndPreferencesToPhoneNumber < ActiveRecord::Migration
+class AddProviderEmailToPhoneNumber < ActiveRecord::Migration
   def self.up
     add_column :phone_numbers, :provider_email, :string
     add_column :phone_numbers, :gateway_preference, :string
