f21ffd895f887130d6ccda5f72d398c78261b327
K. Clair <kclair@kclair-laptop.(none)>
2010-06-15 12:36:39 -0400

    started basic form for list config updates which uses lists controller instead of admin controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,53 @@
+class ListsController < ApplicationController
+  before_filter :require_admin
+  before_filter :get_list, :except => [:new, :create] 
+
+  def new
+  end
+  
+  def create
+    # not used yet
+  end
+  
+  def show
+  end
+
+  def edit
+    if !@list.nil?
+      if request.xhr?
+        render :update do |page|
+          page.replace_html 'col2', :partial => 'lists/edit'
+          page.hide 'flash_messages_container'
+        end
+      end
+   end  
+  end
+  
+  def update
+    if @list.update_attributes(params[:list])
+       render :update do |page|
+         page.replace_html "flash_messages_container", "The list has been updated!"
+         page.show "flash_messages_container"
+       end
+    else
+      render :action => :edit
+    end
+  end
+
+  private
+    def get_list
+      @list = List.find(params[:list_id])
+      if @list.nil?
+        flash[:notice] = "No list selected"
+        if request.xhr?
+           render :update do |page|
+             page.replace_html 'flash_messages_container', flash[:notice] 
+             page.show 'flash_messages_container'
+           end
+        else
+          redirect_to :controller => 'admin', :action => 'manage'
+        end
+      end
+    end
+
+end
