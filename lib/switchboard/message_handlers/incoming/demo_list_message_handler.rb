66a579ff047d2b2cc3e493a660f8a4dbab2fc6cf
josh <josh@durga.serve.com>
2010-06-16 06:50:56 +0400

    supporting list config options

--- lib/switchboard/message_handlers/incoming/demo_list_message_handler.rb
+++ lib/switchboard/message_handlers/incoming/demo_list_message_handler.rb
@@ -50,20 +50,28 @@ module Switchboard::MessageHandlers::Incoming
         if list.has_number?(num) 
           create_outgoing_message( num, message.to, "It seems like you are trying to join the list '" + list_name + "', but you are already a member.")
         else
-          message.list = list
-          num.user = User.new
-          list.add_phone_number(num)
-          message.sender = num.user
-          message.save
-          welcome_message = "u have joined the text message list called '" + list_name + "'!"
-          if list.use_welcome_message?:
-            puts "this list uses the welcome message"
-            welcome_message = list.custom_welcome_message
-          else
-            welcome_message = "no custom, but: " + list.custom_welcome_message
-          end
+          if (list.open_membership)
+            message.list = list
+            if (num.user == nil)
+              puts "adding user for num: " + num.number
+              num.user = User.create(:password => 'abcdef981', :password_confirmation => 'abcdef981')
+              num.user.first_name = 'Unknown'
+              num.save
+              num.user.save
+            end
+            list.add_phone_number(num)
+            message.sender = num.user
+            message.save
+            welcome_message = "You have joined the text message list called '" + list_name + "'!"
+            if list.use_welcome_message?:
+              puts "this list uses the welcome message"
+              welcome_message = list.custom_welcome_message
+            end
 
-          create_outgoing_message( num, message.to, welcome_message ) 
+            create_outgoing_message( num, message.to, welcome_message ) 
+          else ## not list.open_membership
+            create_outgoing_message( num, message.to, "I'm sorry, but this list is configured as a private list and only the administrator can add new members.")
+          end
         end
         handled_state.messages.push(message)
       end
@@ -71,6 +79,9 @@ module Switchboard::MessageHandlers::Incoming
       if (tokens.length > 1) 
         if List.exists?({:name => list_name}) 
           list = List.find_by_name(list_name)
+          message.list = list
+          message.sender = num.user 
+          message.save
           list.phone_numbers.each do |phone_number|
             body = '[' + list_name + '] ' + tokens.join(' ')
             puts "sending message: " + body + ", to: " + phone_number.number
@@ -78,7 +89,7 @@ module Switchboard::MessageHandlers::Incoming
           end
           handled_state.messages.push(message)
         else 
-          create_outgoing_message(num, message_sender, "It seems like you were trying to send a message to the list called " + list_name + ", but no one has started that list yet.") unless message.from_web?
+          create_outgoing_message(num, message_sender, "I'm sorry but I'm not sure what list you're trying to reach!" ) unless message.from_web?
           handled_state.messages.push(message)
         end
       end 
