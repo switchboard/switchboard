cf2ba611a625eec6be0205395edea396f84e6296
josh <josh@durga.serve.com>
2010-06-16 20:36:17 +0400

    reverting

--- db/migrate/20100616003433_add_list_id_to_messages.rb
+++ db/migrate/20100616003433_add_list_id_to_messages.rb
@@ -1,10 +1,9 @@
 class AddListIdToMessages < ActiveRecord::Migration
   def self.up
-  ## i think this should already have been added in a previous migration
- #   add_column :messages, :list_id, :int
+    add_column :messages, :list_id, :int
   end
 
   def self.down
- #   remove_column :messages, :list_id
+    remove_column :messages, :list_id
   end
 end
