6ebe123949164d2919ea5fa0f19fc013a8dc2aeb
K. Clair <kclair@kclair-laptop.(none)>
2010-07-29 13:15:59 -0400

    capify; modify config for server

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :application, "Switchboard"
+set :repository,  "ssh://gitosis@durga.serve.com:2020/mmp-sms.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "mmp.vpscustomer.com"                          # Your HTTP server, Apache/etc
+role :app, "mmp.vpscustomer.com"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+set :user, "switchboard"
+
+set :deploy_to, "/usr/local/switchboard"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
