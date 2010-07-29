890d586cb8f44c466dbcdf367a003b301578ba94
K. Clair <kclair@kclair-laptop.(none)>
2010-07-29 13:27:42 -0400

    disable sudo

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,15 +12,17 @@ role :db,  "localhost", :primary => true # This is where Rails migrations will r
 set :user, "switchboard"
 
 set :deploy_to, "/usr/local/switchboard"
+set :use_sudo, false 
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
