9f31218742738279d4a6da7919dc099d330fd235
K Clair <kclair@pickledradish.com>
2010-12-06 15:01:24 -0700

    add edit user form and icon to link to it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 #  before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update]
+  before_filter :require_admin, :only => [:show, :edit, :update]
   layout 'admin'
   
   def new
@@ -24,20 +24,18 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = @current_user
+    @user = User.find(params[:id]) 
   end
 
   def edit
-    @user = @current_user
+    @user = User.find(params[:id])
   end
   
   def update
-    @user = @current_user # makes our views "cleaner" and more consistent
+    @user = User.find(params[:id]) # makes our views "cleaner" and more consistent
     if @user.update_attributes(params[:user])
-      flash[:notice] = "Account updated!"
-      redirect_to account_url
-    else
-      render :action => :edit
+      flash[:notice] = "User updated!"
     end
+    render :action => :edit
   end
 end
