a0e3cf5a127e7544dc77f3daf6d22429348962b3
Josh Marcus <josh.marcus@gmail.com>
2010-12-10 00:40:24 -0500

    integrating submodule

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,10 @@ set :use_sudo, false
 # these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
-   task :start do ; end
+   task :start do
+      run "git submodule init"
+      run "git submodule update"
+   end
    task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
      #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
