c454091102e5e54fc86b5ce59fbd5a218a858950
Josh Marcus <josh.marcus@gmail.com>
2011-03-14 22:37:32 -0400

    create initial message states in db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Major.create(:name => 'Daley', :city => cities.first)
 user = User.find_or_create_by_login(:login => 'admin', :password => 'admin!', :password_confirmation => 'admin!', :email => 'foo@bar.com', :admin => 1)
 user.save!
+
+# create initial message states
+MessageState.find_or_create_by_name('outgoing');
+MessageState.find_or_create_by_name('incoming');
+MessageState.find_or_create_by_name('handled');
+MessageState.find_or_create_by_name('sent');
