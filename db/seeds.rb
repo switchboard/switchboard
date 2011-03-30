1e290e114222889f75059836c4fbae0cabeac3c0
Josh Marcus <josh.marcus@gmail.com>
2011-03-29 22:53:19 -0400

    added two new message states for incoming and outgoing failed messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,5 @@ MessageState.find_or_create_by_name('outgoing');
 MessageState.find_or_create_by_name('incoming');
 MessageState.find_or_create_by_name('handled');
 MessageState.find_or_create_by_name('sent');
+MessageState.find_or_create_by_name('error_incoming');
+MessageState.find_or_create_by_name('error_outgoing');
