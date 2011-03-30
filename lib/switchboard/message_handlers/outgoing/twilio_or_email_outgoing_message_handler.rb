91aad1a5cc3bc253b62620ae1e49933efcdab381
Josh Marcus <josh.marcus@gmail.com>
2011-03-29 22:52:56 -0400

    put failed messages in error status

--- lib/switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb
+++ lib/switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb
@@ -8,8 +8,12 @@ module Switchboard::MessageHandlers::Outgoing
             sender = Twilio::TwilioSender.new()
             ## these should come from an array of output connectors (tuple of state & conditions)
             output_state_name = 'sent'
+            outgoing_error_state_name = 'error_outgoing'
+
             output_state_conditions = {}
             outgoing_state = MessageState.find_by_name(output_state_name)
+            error_state = MessageState.find_by_name(outgoing_error_state_name)
+
             outgoing_states = [ outgoing_state ]
 
             process_count = 0
@@ -19,23 +23,27 @@ module Switchboard::MessageHandlers::Outgoing
               ## messages go out slowly, so we will fork a new process for each
               ## message.  We send them out in batches of 30 until we test what
               ## limit is reasonable.
-              fork {
-                puts "handling outgoing message."
-                if (message.to == 'Web') 
-                  puts "WARNING: incorrect messages are being generated to Web"
-                else
-                  if ( message.respond_to? :carrier) 
-                    puts "emailing message"
-                    send_email  message.to, :body => message.body, :from => message.from
-                  else 
-                    puts "texting response"
-                    sender.send_sms( message.to, message.body )
+              begin
+                fork {
+                  puts "handling outgoing message."
+                  if (message.to == 'Web') 
+                    puts "WARNING: incorrect messages are being generated to Web"
+                  else
+                    if ( message.respond_to? :carrier) 
+                      puts "emailing message"
+                      send_email  message.to, :body => message.body, :from => message.from
+                    else 
+                      puts "texting response"
+                      sender.send_sms( message.to, message.body )
+                    end
                   end
-                end
                 }
                 outgoing_state.messages.push(message) 
                 outgoing_state.save
-
+                rescue
+                  error_state.messages.push(message)
+                  error_state.save                  
+                end
                 process_count += 1
                if process_count == 30:
                   ## wait for all child processes to finish
