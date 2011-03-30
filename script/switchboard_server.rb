f14d9254ffa6f22c1bec0970f3b20ebb6b5eb5c9
Josh Marcus <josh.marcus@gmail.com>
2011-03-29 22:53:49 -0400

    remove state creation from server script (now in db:seed task)

--- script/switchboard_server.rb
+++ script/switchboard_server.rb
@@ -20,11 +20,6 @@ SLEEP_TIME = 10
 #end
 print("Running switchboard server.\n")
 
-outgoing_state = MessageState.find_or_create_by_name('outgoing');
-incoming_state = MessageState.find_or_create_by_name('incoming');
-outgoing_state = MessageState.find_or_create_by_name('handled');
-outgoing_state = MessageState.find_or_create_by_name('sent');
-
 require 'switchboard/message_handlers/incoming/demo_list_message_handler.rb'
 require 'switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb'
 
