4692fe7877f01563496cda16414258a2358e48a3
Josh Marcus <josh.marcus@gmail.com>
2011-06-19 16:17:40 -0400

    setting production setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,21 @@
 set :application, "Switchboard"
-set :repository,  ""
+set :repository,  "git://github.com/switchboard/switchboard.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-set :branch, "production"
+set :branch, "master"
 
-role :web, ""                          # Your HTTP server, Apache/etc
-role :app, ""                          # This may be the same as your `Web` server
-role :db,  "", :primary => true # This is where Rails migrations will run
+
+role :web, "69.164.216.141"                          # Your HTTP server, Apache/etc
+role :app, "69.164.216.141"                          # This may be the same as your `Web` server
+role :db,  "69.164.216.141", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set :user, "switchboard"
 
-set :deploy_to, ""
-set :use_sudo, false 
+set :deploy_to, "/home/switchboard/production"
+set :use_sudo, false
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
