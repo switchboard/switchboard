1fcdb004dc8a55b5afcc769ba88abbd0b5cf394c
Josh Marcus <josh.marcus@gmail.com>
2012-02-12 17:09:07 -0500

    Clean up outgoing message handler.

--- lib/switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb
+++ lib/switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb
@@ -21,7 +21,7 @@ module Switchboard::MessageHandlers::Outgoing
 
       process_count = 0
 
-      messages_to_handle.each do |msg|
+      messages_to_handle.each do |message|
         ## messages go out slowly, so we will fork a new process for each
         ## message.  We send them out in batches of 30 until we test what
         ## limit is reasonable.
@@ -29,63 +29,61 @@ module Switchboard::MessageHandlers::Outgoing
         ## turned off forking because of mysql connection
         #threads = []
         #threads << Thread.new(msg) { |message| 
-          begin
-            puts "handling outgoing message."
-            if (message.to == 'Web') 
-              puts "WARNING: incorrect messages are being generated to Web"
-            else
-              if ( message.respond_to? :carrier) 
-                puts "emailing message"
-                send_email  message.to, :body => message.body, :from => message.from
-              else 
-                if (message.from == nil || message.from == '') 
-                  #TODO: system wide settings 
-                  puts("sending from sender: system wide sender")
-                  sender.send_sms( message.to, message.body)
-                else
-                  puts("sending from sender: " + message.from )
-                  sender.send_sms( message.to, message.body, message.from)
-                end
+        begin
+          puts "handling outgoing message."
+          if (message.to == 'Web') 
+            puts "WARNING: incorrect messages are being generated to Web"
+          else
+            if ( message.respond_to? :carrier) 
+              puts "emailing message"
+              send_email  message.to, :body => message.body, :from => message.from
+            else 
+              if (message.from == nil || message.from == '') 
+                #TODO: system wide settings 
+                puts("sending from sender: system wide sender")
+                sender.send_sms( message.to, message.body)
+              else
+                puts("sending from sender: " + message.from )
+                sender.send_sms( message.to, message.body, message.from)
               end
             end
-            message.message_state = sent_state 
-            message.save
-          rescue StandardException => e
-            puts("outgoing messages -- failure")
-            puts("error was: " + e.inspect )
-            puts("error backtrace: " + e.backtrace.inspect )
-            puts("error was: " + e.inspect )
-            puts("e: " + e.to_s )
-
-            error_state.messages.push(message)
-            error_state.save                  
           end
+          message.message_state = sent_state 
+          message.save
+        rescue Exception => e
+          puts("outgoing messages -- failure")
+          puts("error was: " + e.inspect )
+          puts("error backtrace: " + e.backtrace.inspect )
+          puts("error was: " + e.inspect )
+          puts("e: " + e.to_s )
+          error_state.messages.push(message)
+          error_state.save                  
+        end
         #}
         #threads.each { |aThread| aThread.join }
       end
     end
  
-def send_email(to,opts={})
-  opts[:server]      ||= 'localhost'
-  opts[:from]        ||= ''
-  opts[:from_alias]  ||= 'Switchboard'
-  opts[:subject]     ||= ""
-  opts[:body]        ||= ""
- 
-  msg = <<END_OF_MESSAGE
+    def send_email(to,opts={})
+      opts[:server]      ||= 'localhost'
+      opts[:from]        ||= ''
+      opts[:from_alias]  ||= 'Switchboard'
+      opts[:subject]     ||= ""
+      opts[:body]        ||= ""
+     
+      msg = <<END_OF_MESSAGE
 From: #{opts[:from_alias]} <#{opts[:from]}>
 To: <#{to}>
 Subject: #{opts[:subject]}
- 
+     
 #{opts[:body]}
 END_OF_MESSAGE
- 
-  Net::SMTP.start(opts[:server]) do |smtp|
-    smtp.send_message msg, opts[:from], to
-  end
-end
-
+     
+      Net::SMTP.start(opts[:server]) do |smtp|
+        smtp.send_message msg, opts[:from], to
+      end
     end
+  end
 end
 
 
