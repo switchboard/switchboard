e3820d1bd1cd3a22e27264ef6cb3e5f544c83a24
Bennet Huber <bhuber@azavea.com>
2012-04-28 13:51:47 -0400

    Add some validation to survey_question

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -1,2 +1,7 @@
 class SurveyQuestion < ActiveRecord::Base
+
+  belongs_to      :survey
+  validates       :name,          :presence => true
+  validates       :question_text, :presence => true
+  validates       :survey_id,     :presence => true
 end
