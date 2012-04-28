1ebb6ff470cc9ae3c6dc37ca9301cc5518338a0e
Dan Feder <dafeder@gmail.com>
2012-04-28 15:08:49 -0400

    Add position column to survey questions

---
+++ db/migrate/20120428185558_add_position_to_survey_questions.rb
@@ -0,0 +1,9 @@
+class AddPositionToSurveyQuestions < ActiveRecord::Migration
+  def self.up
+    add_column :survey_questions, :position, :integer
+  end
+
+  def self.down
+    remove_column :survey_states, :position
+  end
+end
