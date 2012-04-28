dccaa2ad99dd17e6e86fd06444b94ce437d3997c
Dan Feder <dafeder@gmail.com>
2012-04-28 14:26:44 -0400

    Validation for survey states

--- app/models/survey_state.rb
+++ app/models/survey_state.rb
@@ -1,5 +1,9 @@
 class SurveyState < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :phone_number
   belongs_to :survey
   belongs_to :survey_question
+
+  validates_presence_of :survey_id
+  validates_presence_of :survey_question_id
+  validates_presence_of :phone_number_id
 end
