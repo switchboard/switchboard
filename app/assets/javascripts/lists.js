c6666b60b91abdefc1e7132e9f193ec9b56f0e33
Lorenzo Manacorda <lorenzomanacorda@gmail.com>
2012-07-23 20:51:36 +0200

    fix welcome message toggle

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,14 +1,19 @@
-$(function($) {
-    $("#list_name").change(function() {
- 	alert("ok, name is changed");	
-        $.ajax({url: '/admin/check_list_availability',
-        	data: 'name=' + this.value,
-		      "success": switchboard.check_list_name_ok 
-        	})
-    });
+$(function() {
+  // check availability of list name
+  $("#list_name").change(function() {
+    $.ajax({url: '/admin/check_list_availability',
+      data: 'name=' + this.value,
+      "success": switchboard.check_list_name_ok
+      });
+  });
+
+  // toggle welcome message textarea
+  $('#list_use_welcome_message').change(function() {
+    $('#welcome_message').toggle();
+  });
 });
 
-switchboard = {}
+switchboard = {};
 
 switchboard.check_list_name_ok = function(data, status, jqxhr) {
   console.log("received list name availability response");
