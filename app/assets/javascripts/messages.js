ca4233c15ead297f5309279123410efdab6671f1
Lorenzo Manacorda <lorenzomanacorda@gmail.com>
2012-07-25 23:56:29 +0200

    remove js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -3,26 +3,4 @@ $(function() {
   $('#message_body_textarea').keyup(function() {
     countMessageBody(this);
   });
-
-  // send message
-  $('#send-button').click(function() {
-    var $spinner = $('#send_message_spinner');
-
-    var list_id = $(this).data('list_id');
-    var data = {
-      list_id: $(this).data('list_id'),
-      message_body: $('#message_body_textarea').val(),
-      confirmed: false
-    };
-
-    $spinner.show();
-
-    $.post('/send_message', data).
-    success(function(response) {
-      alert(response);
-    }).
-    always(function() {
-      $('#send_message_spinner').hide();
-    });
-  });
 });
