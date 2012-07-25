15ffea74eaec4e356eee237274563e48e7525baf
Lorenzo Manacorda <lorenzomanacorda@gmail.com>
2012-07-25 22:00:56 +0200

    send message js functionality

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,25 @@
 $(function() {
+  // count chars
   $('#message_body_textarea').keyup(function() {
     countMessageBody(this);
+  });
+
+  // send message
+  $('#send-button').click(function() {
+    var $spinner = $('#send_message_spinner');
+
+    var list_id = $(this).data('list_id');
+    var data = {
+      list_id: $(this).data('list_id'),
+      message_body: $('#message_body_textarea').val(),
+      confirmed: false
+    };
+
+    $spinner.show();
+
+    $.post('/admin/send_message', data).
+      always(function() {
+        $('#send_message_spinner').hide();
+      });
   });
 });
