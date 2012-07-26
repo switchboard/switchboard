10e7f9b8d73e87e3383117672d9d4f273b1df939
Josh Marcus <jmarcus@azavea.com>
2012-07-26 08:34:46 -0400

    Make phone_numbers_attributes accessible for update.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :phone_numbers
   attr_accessible :first_name, :last_name, :email
+  attr_accessible :phone_numbers_attributes
 
   acts_as_authentic do |c|
     #c.validate_password_field = false  <--- this breaks the creation of seed data
