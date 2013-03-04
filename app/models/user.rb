406eab7ec1044527f3da8ad791e5b2e0d5f5e001
David Reese <david@whatcould.com>
2013-03-03 22:27:24 -0500

    Cleanup user#full_name
    Ruby!

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,19 +30,7 @@ class User < ActiveRecord::Base
     self.admin
   end
 
-  def full_name 
-    puts("generating full name")
-    name = ''
-    if ( ! @first_name.blank? ) 
-      name += @first_name
-    end
-
-    if ( ! @last_name.blank? )
-      ## add a space if necessary
-      name += ( name != '' ? ' ' : '')
-      name += @last_name
-    end
-    puts("name: " + name)
-    name
+  def full_name
+    [first_name, last_name].join(' ')
   end
 end
