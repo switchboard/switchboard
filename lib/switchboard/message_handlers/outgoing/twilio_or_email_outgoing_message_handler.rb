d507941f44d872fde03f722de7b88152804ea5d1
David Reese <david@whatcould.com>
2013-09-25 13:13:19 -0400

    Remove splitting message from message handler
    Now just handling it earlier

--- lib/switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb
+++ lib/switchboard/message_handlers/outgoing/twilio_or_email_outgoing_message_handler.rb
@@ -1,7 +1,7 @@
 require 'switchboard/message_handlers/outgoing_message_handler.rb'
 require 'twilio/twilio_sender'
 require 'net/smtp'
- 
+
 module Switchboard::MessageHandlers::Outgoing
   class TwilioOrEmailOutgoingMessageHandler < Switchboard::MessageHandlers::OutgoingMessageHandler
     def handle_messages!()
@@ -17,7 +17,7 @@ module Switchboard::MessageHandlers::Outgoing
 
       ##outgoing_states = [ outgoing_state ]
 
-      ## NOTE: in generic model, change to outgoing_states.each |state,conditions| do 
+      ## NOTE: in generic model, change to outgoing_states.each |state,conditions| do
 
       process_count = 0
 
@@ -28,18 +28,18 @@ module Switchboard::MessageHandlers::Outgoing
 
         ## turned off forking because of mysql connection
         #threads = []
-        #threads << Thread.new(msg) { |message| 
+        #threads << Thread.new(msg) { |message|
         begin
           puts "handling outgoing message."
-          if (message.to == 'Web') 
+          if (message.to == 'Web')
             puts "WARNING: incorrect messages are being generated to Web"
           else
-            if ( message.respond_to? :carrier) 
+            if ( message.respond_to? :carrier)
               puts "emailing message"
               send_email  message.to, :body => message.body, :from => message.from
-            else 
-              if (message.from == nil || message.from == '') 
-                #TODO: system wide settings 
+            else
+              if (message.from == nil || message.from == '')
+                #TODO: system wide settings
                 puts("sending from sender: system wide sender")
                 send_split_message(sender, message.to, message.body)
               else
@@ -48,7 +48,7 @@ module Switchboard::MessageHandlers::Outgoing
               end
             end
           end
-          message.message_state = sent_state 
+          message.message_state = sent_state
           message.save
         rescue Exception => e
           puts("outgoing messages -- failure")
@@ -57,22 +57,15 @@ module Switchboard::MessageHandlers::Outgoing
           puts("error was: " + e.inspect )
           puts("e: " + e.to_s )
           error_state.messages.push(message)
-          error_state.save                  
+          error_state.save
         end
         #}
         #threads.each { |aThread| aThread.join }
       end
     end
 
-    def send_split_message(sender, to, body, from = nil)
-      if body.length <= 160
-        sender.send_sms(to, body, from)
-      else
-        chunks = split_message_by_160(body)
-        chunks.each do |chunk|
-          sender.send_sms(to, chunk, from)
-        end
-      end
+    def send_message(sender, to, body, from = nil)
+      sender.send_sms(to, body, from)
     end
 
     def split_message_by_160(str)
@@ -92,15 +85,15 @@ module Switchboard::MessageHandlers::Outgoing
       opts[:from_alias]  ||= 'Switchboard'
       opts[:subject]     ||= ""
       opts[:body]        ||= ""
-     
+
       msg = <<END_OF_MESSAGE
 From: #{opts[:from_alias]} <#{opts[:from]}>
 To: <#{to}>
 Subject: #{opts[:subject]}
-     
+
 #{opts[:body]}
 END_OF_MESSAGE
-     
+
       Net::SMTP.start(opts[:server]) do |smtp|
         smtp.send_message msg, opts[:from], to
       end
