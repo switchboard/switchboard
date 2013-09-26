8fb67ae2bb36d719d2ddc322d420d4f9b76b3f53
David Reese <david@whatcould.com>
2013-09-26 12:20:02 -0400

    Notify Airbrake on error when sending message

--- lib/switchboard/message_handlers/incoming/demo_list_message_handler.rb
+++ lib/switchboard/message_handlers/incoming/demo_list_message_handler.rb
@@ -8,10 +8,10 @@ module Switchboard::MessageHandlers::Incoming
     ## Methods:
     ## 1) The recipient phone number has a single list assigned to it.
     ## 2) Email messages have an assigned list (each address specifies a list)
-    ## 3) Message from the web interface have the list set as message.to  
+    ## 3) Message from the web interface have the list set as message.to
     ## 4) Messages to a keyword list are assumed to have the first token set to the
     ##    list name
-    ##  
+    ##
     ## For the future: ?TODO: create model for incoming numbers/gateways/channels/etc.
     ## -- Incoming numbers should have their own model
     ## -- Give numbers that handle messages by keyword a default list
@@ -26,7 +26,7 @@ module Switchboard::MessageHandlers::Incoming
           ## Non-keyword list (assigned phone number)
         elsif message.respond_to? :carrier
           ## Email message for a list
-          list_name = message.default_list 
+          list_name = message.default_list
           list = List.find_by_name(message.default_list)
         elsif message.from_web?
           ## Web message
@@ -59,8 +59,8 @@ module Switchboard::MessageHandlers::Incoming
           number_string = message.sender_number
           number_string.sub!("+1", "")
           puts "Message is from number: " + number_string
-      
-          num = PhoneNumber.find_or_create_by_number( number_string ) 
+
+          num = PhoneNumber.find_or_create_by_number( number_string )
           num.save
 
           survey_state = SurveyState.where(active: true, phone_number_id: 1).first
@@ -73,22 +73,22 @@ module Switchboard::MessageHandlers::Incoming
             next
           end
 
-          list = determine_list(message) 
-          if ( list == nil ) 
+          list = determine_list(message)
+          if ( list == nil )
             puts("couldn't determine list")
             Rails.logger.warn("couldn't determine list")
             error_state.messages.push(message)
             error_state.save
             next
-          end 
-
-          ##?REVIEW: handle this more elegantly 
-          ##If the message comes via email, save the carrier address.
-          if (message.respond_to? :carrier)  
-           num.provider_email = message.carrier 
           end
 
-          if ( message.tokens.count > 0 ) 
+          ##?REVIEW: handle this more elegantly
+          ##If the message comes via email, save the carrier address.
+          if (message.respond_to? :carrier)
+           num.provider_email = message.carrier
+          end
+
+          if ( message.tokens.count > 0 )
             first_token = message.tokens[0]
           else
             Rails.logger.warn("empty message")
@@ -97,11 +97,11 @@ module Switchboard::MessageHandlers::Incoming
             error_state.save
             next
           end
-        
+
           if ( first_token =~ /^join$/i )  ## join action
              list.handle_join_message(message, num)
           elsif ( first_token =~ /^leave$/i or first_token =~ /^quit$/i )  ## quit action
-            ##?TODO: move to list model 
+            ##?TODO: move to list model
             Rails.logger.info("Received list quit message")
             list.create_outgoing_message( num, "You have been removed from the " + list.name + " list, as you requested." )
             list.remove_phone_number(num)
@@ -114,11 +114,7 @@ module Switchboard::MessageHandlers::Incoming
            handled_state.messages.push(message)
            handled_state.save
         rescue StandardError => e
-          puts("exception while processing message")
-          puts("error was: " + e.inspect )
-          puts("error backtrace: " + e.backtrace.inspect )
-          puts("error was: " + e.inspect )
-          puts("e: " + e.to_s )
+          Airbrake.notify_or_ignore(e)
 
           Rails.logger.warn("exception while processing message")
           Rails.logger.warn("error was: " + e.inspect )
