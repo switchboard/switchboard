2a182686a83f120200fa78a9f337c214c4982b9b
David Reese <david@whatcould.com>
2013-10-23 18:04:56 -0400

    Registration controller does not require_user

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,10 +1,12 @@
 class RegistrationController < ApplicationController
+  skip_before_filter :require_user
+
   layout 'login'
 
   def new_invited
     @invitation = Invitation.find_by_token(params[:invitation_token])
     if ! @invitation
-      redirect_to signin_path, alert: 'That invitation cannot be found; did you already sign up? Check your link again, or request a new invitation.' and return 
+      redirect_to signin_path, alert: 'That invitation cannot be found; did you already sign up? Check your link again, or request a new invitation.' and return
     elsif current_user
       redirect_to lists_path, notice: "You're already signed-in to Switchboard!"
     end
