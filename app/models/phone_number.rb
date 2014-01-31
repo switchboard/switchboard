c15954a20edf0e9554c0880466adde8cdb657f8b
David Reese <david@whatcould.com>
2014-01-31 13:52:23 -0500

    Remove phone number validation

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -5,7 +5,7 @@ class PhoneNumber < ActiveRecord::Base
 
   attr_accessible :number
 
-  validates :number, uniqueness: true, format: {with: /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/, message: 'This phone number does not look valid.'}
+  validates :number, uniqueness: true, length: {is: 10, message: 'This phone number does not look valid.'}
 
   def add_to_list(list)
     self.list_memberships.create! :list => list
