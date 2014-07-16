41c2d82da1f5e9348b7088979ec4e0ddaa96b9dc
David Reese <david@whatcould.com>
2014-07-16 12:19:34 -0400

    Fix twilio test to handle new syntax
    Switched Twilio methods

--- test/unit/twilio_sender_test.rb
+++ test/unit/twilio_sender_test.rb
@@ -6,16 +6,16 @@ class TwilioSenderTest < ActiveSupport::TestCase
     @from = '2151001000'
     @body = 'Test this SMS'
     # AKA stub_chain
-    TwilioSender.stubs(twilio_client: stub(account: stub(sms: stub(messages: stub()))))
+    TwilioSender.stubs(twilio_client: stub(account: stub(messages: stub())))
   end
 
   test 'adds +1 onto numbers and sends sms' do
-    TwilioSender.twilio_client.account.sms.messages.expects(:create).with(from: "+1#{@from}", to: "+1#{@to}", body: @body)
+    TwilioSender.twilio_client.account.messages.expects(:create).with(from: "+1#{@from}", to: "+1#{@to}", body: @body)
     TwilioSender.send_sms(@to, @body, @from)
   end
 
   test 'does not add +1 when numbers already have it' do
-    TwilioSender.twilio_client.account.sms.messages.expects(:create).with(from: "+1#{@from}", to: "+1#{@to}", body: @body)
+    TwilioSender.twilio_client.account.messages.expects(:create).with(from: "+1#{@from}", to: "+1#{@to}", body: @body)
     TwilioSender.send_sms("+1#{@to}", @body, "+1#{@from}")
   end
 
