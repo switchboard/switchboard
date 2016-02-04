e8c01d9f7a35a2a3289b66f8e3937ae6fa2c36df
David Reese <david@whatcould.com>
2016-02-04 17:23:49 -0500

    Check for join/leave commands correctly
    Flip order of assignment inside conditional (was setting locale to true)

--- app/models/message.rb
+++ app/models/message.rb
@@ -101,10 +101,10 @@ class Message < ActiveRecord::Base
   def process
     begin
       first_token = tokens.first.downcase
-      if locale = I18n.locale_for(key: 'list_commands.join', val: first_token) && tokens.length < 3
+      if tokens.length < 4 && locale = I18n.locale_for(key: 'list_commands.join', val: first_token)
         list.handle_join_message(self, locale)
         mark_handled!
-      elsif locale = I18n.locale_for(key: 'list_commands.leave', val: first_token) && tokens.length < 3
+      elsif tokens.length < 4 && locale = I18n.locale_for(key: 'list_commands.leave', val: first_token)
         list.handle_leave_message(self, locale)
         mark_handled!
       elsif list.can_send_message?(self)
