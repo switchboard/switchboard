46d0d093e8032b5fe5a986c9d0b93d642c2ccb11
David Reese <david@whatcould.com>
2016-02-13 18:21:21 -0500

    Handle references to removed model MessageState in migrations

--- db/migrate/20130428135151_add_message_aasm.rb
+++ db/migrate/20130428135151_add_message_aasm.rb
@@ -6,14 +6,15 @@ class AddMessageAasm < ActiveRecord::Migration
   def up
     add_column :messages, :aasm_state, :string
     add_index :messages, [:aasm_state], name: 'index_messages_on_aasm_state'
-
-    Message.reset_column_information
-    Message.where(message_state_id: MessageState.find_by_name('outgoing')).update_all(aasm_state: 'old_outgoing')
-    Message.where(message_state_id: MessageState.find_by_name('incoming')).update_all(aasm_state: 'incoming')
-    Message.where(message_state_id: MessageState.find_by_name('handled')).update_all(aasm_state: 'sent')
-    Message.where(message_state_id: MessageState.find_by_name('sent')).update_all(aasm_state: 'sent')
-    Message.where(message_state_id: MessageState.find_by_name('error_incoming')).update_all(aasm_state: 'old_error_incoming')
-    Message.where(message_state_id: MessageState.find_by_name('error_outgoing')).update_all(aasm_state: 'old_error_outgoing')
+    if defined?(MessageState)
+      Message.reset_column_information
+      Message.where(message_state_id: MessageState.find_by_name('outgoing')).update_all(aasm_state: 'old_outgoing')
+      Message.where(message_state_id: MessageState.find_by_name('incoming')).update_all(aasm_state: 'incoming')
+      Message.where(message_state_id: MessageState.find_by_name('handled')).update_all(aasm_state: 'sent')
+      Message.where(message_state_id: MessageState.find_by_name('sent')).update_all(aasm_state: 'sent')
+      Message.where(message_state_id: MessageState.find_by_name('error_incoming')).update_all(aasm_state: 'old_error_incoming')
+      Message.where(message_state_id: MessageState.find_by_name('error_outgoing')).update_all(aasm_state: 'old_error_outgoing')
+    end
   end
 
   def down
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,1 @@
 
-# create initial message states
-
-# These are deprecated!
-
-# MessageState.find_or_create_by_name('outgoing');
-# MessageState.find_or_create_by_name('incoming');
-# MessageState.find_or_create_by_name('handled');
-# MessageState.find_or_create_by_name('sent');
-# MessageState.find_or_create_by_name('error_incoming');
-# MessageState.find_or_create_by_name('error_outgoing');
